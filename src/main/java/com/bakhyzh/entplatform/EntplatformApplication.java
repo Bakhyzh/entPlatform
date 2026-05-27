923fa0a90a871a437b5f41d38c9739ee57d5ae4a
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-05-27 18:56:18 +0500

    Initial project setup - ENT platform

---
+++ src/main/java/com/bakhyzh/entplatform/EntplatformApplication.java
@@ -0,0 +1,13 @@
+package com.bakhyzh.entplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakhyzh/entplatform/EntplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakhyzh.entplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
