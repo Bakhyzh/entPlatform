c8d29067338d1bc458598b4ab8496623af33a366
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-05-31 12:51:35 +0500

    new feat change register

--- src/main/java/com/bakhyzh/entplatform/auth/controller/RegisterController.java
+++ src/main/java/com/bakhyzh/entplatform/auth/controller/RegisterController.java
@@ -1,8 +1,10 @@
 package com.bakhyzh.entplatform.auth.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/register")
 public class RegisterController {
 
 }
