4b284a6a334e60badcea56c86c5879ef31089f95
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-06-04 00:12:01 +0500

    add UniversalController change #4

--- src/main/java/com/bakhyzh/entplatform/auth/controller/UniversalController.java
+++ src/main/java/com/bakhyzh/entplatform/auth/controller/UniversalController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,7 +13,8 @@ import java.util.List;
 public class UniversalController {
     @GetMapping("/")
     public ResponseEntity<List<String>> getall(){
-        return null;
+        List<String> lists = new ArrayList<>();
+        return lists;
     }
 
 }
